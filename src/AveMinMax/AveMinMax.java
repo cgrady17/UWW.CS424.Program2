33709b2a5f542a1531fc7511d109d754ea6fa7b0
Connor Grady <cgrady17@outlook.com>
2016-02-22 14:53:01 -0600

    Initial commit

---
+++ src/AveMinMax/AveMinMax.java
@@ -0,0 +1,20 @@
+package AveMinMax;
+
+/**
+ * UWW CS 424 | Spring 2016 | Program 2
+ * Multithreaded program that calculates various statistical values
+ * from a list of numbers. Each value calculation will be performed
+ * on its own thread. The primary thread then outputs the values.
+ *
+ * @author Connor P Grady (1765561)
+ * @version 1.0
+ */
+public class AveMinMax {
+    private int average;
+    private int minimum;
+    private int maximum;
+
+    public AveMinMax() {
+
+    }
+}
